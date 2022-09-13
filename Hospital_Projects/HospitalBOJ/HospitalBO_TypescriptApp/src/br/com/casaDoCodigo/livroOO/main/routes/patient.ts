58a4fac765a36ed0be8b4c0edd93b7340cdf376e
alexandregarskemachado <alexandre.machado@compasso.com.br>
2022-09-13 14:30:28 -0300

    Add node-json-db and data manipulation

--- Hospital_Projects/HospitalBOJ/HospitalBO_TypescriptApp/src/br/com/casaDoCodigo/livroOO/main/routes/patient.ts
+++ Hospital_Projects/HospitalBOJ/HospitalBO_TypescriptApp/src/br/com/casaDoCodigo/livroOO/main/routes/patient.ts
@@ -5,17 +5,16 @@ import { Plan } from "../../entities/Plan";
 import { AddressConversor } from "../../utils/AddressConversor";
 import { DataShow } from "../../utils/DataShow";
 import { DateConversor } from "../../utils/DateConversor";
-
-// let patientBusiness = new PatientBusiness();
-
+import { JsonDB, Config } from "node-json-db";
+import fs from "fs";
 export class PatientRouter {
   public router: Router;
   public patientBusiness: PatientBusiness = new PatientBusiness();
+  private db: any;
 
   public constructor() {
     this.router = Router();
-    // this.patientBusiness = new PatientBusiness();
-    // this.patientBusiness = patientBusiness;
+    this.db = new JsonDB(new Config("hospitalDataBase", true, false, "/"));
   }
 
   private testRoute(req: Request, res: Response) {
@@ -26,12 +25,11 @@ export class PatientRouter {
     }
   }
 
-  public createPatient(req: Request, res: Response): void {
-    console.log("THIS -> ", this)  
+  public async createPatient(req: Request, res: Response): Promise<void> {
     try {
       let patientArray = req.body.patients;
       let patientsArray: Array<Patient> = new Array();
-      patientArray.forEach((patient: any) => {
+      patientArray.forEach(async (patient: any) => {
         let newPatient: Patient = new Patient();
         newPatient.setCpf(patient.cpf);
         newPatient.setName(patient.name);
@@ -46,15 +44,24 @@ export class PatientRouter {
         plan.setName(patient.plan.planName);
         plan.setMonthlyPayment(patient.plan.monthlyPayment);
         newPatient.setPlan(plan);
-        patientsArray.push(newPatient);
-        // this.patientBusiness.add(newPatient);    
-        // console.log(PatientBusiness.prototype)
-        // PatientBusiness.(newPatient)
+        let allPatients = await this.db.getData("/patients");
+        let canAdd = true;
+        allPatients.forEach((patient: { id: any }) => {
+          if (patient.id == newPatient.getCpf()) {
+            canAdd = false;
+          }
+        });
+        if (canAdd) {
+          patientsArray.push(newPatient);
+          await this.db.push(
+            "/patients[]",
+            { id: `${newPatient.getCpf()}`, patient: newPatient },
+            true
+          );
+        }
       });
-
       res.status(200).json({
-        message: "Patient added",
-        patients: patientsArray,
+        message: "Patients added",
       });
     } catch (err) {
       res.status(500).json({
@@ -68,23 +75,38 @@ export class PatientRouter {
     }
   }
 
-  private changePatientAddress(req: Request, res: Response) {
+  private async updatePatient(req: Request, res: Response) {
     try {
       let patient: Patient = new Patient();
       patient.setCpf(req.body.patient.cpf);
+      patient.setName(req.body.patient.name);
       patient.setAddress(
         new AddressConversor().convertAddress(req.body.patient.address)
       );
+      patient.setAddress(
+        new AddressConversor().convertAddress(req.body.patient.address)
+      );
+      patient.setDateOfBirth(
+        new DateConversor().dateConverter(req.body.patient.dateOfBirth)
+      );
 
+      let plan: Plan = new Plan();
+      plan.setName(req.body.patient.plan.planName);
+      plan.setMonthlyPayment(req.body.patient.plan.monthlyPayment);
+      patient.setPlan(plan);
+
+      let content = JSON.parse(fs.readFileSync('hospitalDataBase.json', 'utf-8'));
+      console.log(content);
+      content.patients.forEach((oldPatient: any) => {
+        if (oldPatient.id == patient.getCpf()){
+          oldPatient.patient = patient;
+        }
+      });
+      fs.writeFileSync('hospitalDataBase.json', JSON.stringify(content));
+      await this.db.reload();
       res.status(200).json({
         message: "Patient address updated",
-        patient: {
-          name: patient.getName(),
-          cpf: patient.getCpf(),
-          address: patient.getAddress(),
-          dateOfBirth: patient.getDateOfBirth(),
-          plan: patient.getPlan(),
-        },
+        patient: patient,
       });
     } catch (err) {
       res.status(500).json({
@@ -98,27 +120,27 @@ export class PatientRouter {
     }
   }
 
-  private showPatient(req: Request, res: Response) {
-    try {
-      let query: any = req.query.patient;
-      if (query) {
-        let patient = new DataShow().showPatient(query);
-        res.status(200).json(patient);
+  private async showPatient(req: Request, res: Response) {
+    let query: any = req.query.id;
+    let showPatient = false;
+    let patientData;
+    const patientsArray = await this.db.getData("/patients");
+    patientsArray.forEach(async (patient: { id: any }) => {
+      if (patient.id === query) {
+        showPatient = true;
+        patientData = patient;
       }
-    } catch (err) {
-      res.status(500).json({
-        status: 500,
-        message: [
-          {
-            error: `Error in request ${err}`,
-          },
-        ],
-      });
+    });
+
+    if (showPatient) {
+      res.status(200).json(patientData);
+    } else {
+      res.status(500).json("Error: Patient not found");
     }
   }
-  private showPatients(req: Request, res: Response) {
+  private async showPatients(req: Request, res: Response) {
     try {
-      let patients = this.patientBusiness.showAll();
+      let patients = await this.db.getData("/patients");
       res.status(200).json({ patients: patients });
     } catch (err) {
       res.status(500).json({
@@ -132,21 +154,21 @@ export class PatientRouter {
     }
   }
 
-  private bindThis(): void{
-      this.testRoute = this.testRoute.bind(this);
-      this.showPatient = this.showPatient.bind(this);
-      this.showPatients = this.showPatients.bind(this);
-      this.createPatient = this.createPatient.bind(this);
-      this.changePatientAddress = this.changePatientAddress.bind(this);
+  private bindThis(): void {
+    this.testRoute = this.testRoute.bind(this);
+    this.showPatient = this.showPatient.bind(this);
+    this.showPatients = this.showPatients.bind(this);
+    this.createPatient = this.createPatient.bind(this);
+    this.updatePatient = this.updatePatient.bind(this);
   }
 
   public init(): void {
-    this.bindThis();  
+    this.bindThis();
     this.router.get("/testPatientRouter", this.testRoute);
     this.router.get("/getPatient", this.showPatient);
     this.router.get("/getPatients", this.showPatients);
     this.router.post("/createPatient", this.createPatient);
-    this.router.put("/changePatientAddress", this.changePatientAddress);
+    this.router.put("/updatePatient", this.updatePatient);
   }
 }
 

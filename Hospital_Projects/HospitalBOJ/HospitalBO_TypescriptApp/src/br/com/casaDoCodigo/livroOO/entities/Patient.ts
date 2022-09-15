aa0dea1a6f9ce07e50d764bad9652aa30b5cf58f
alexandregarskemachado <alexandre.machado@compasso.com.br>
2022-09-15 17:42:13 -0300

    refactoring and adding constructor

--- Hospital_Projects/HospitalBOJ/HospitalBO_TypescriptApp/src/br/com/casaDoCodigo/livroOO/entities/Patient.ts
+++ Hospital_Projects/HospitalBOJ/HospitalBO_TypescriptApp/src/br/com/casaDoCodigo/livroOO/entities/Patient.ts
@@ -1,11 +1,17 @@
+import { Address } from "./Address";
 import { Person } from "./Person";
 import { Plan } from "./Plan";
 
 export class Patient extends Person {
   private cpf: string;
   private plan: Plan;
-
-  public Patient() {}
+  
+  public constructor(cpf: string, plan: Plan, name: string, dateOfBirth: Date, address: Address){
+    super(name, dateOfBirth, address);
+    this.cpf = cpf;
+    this.plan = plan
+  }
+  // public Patient() {}
 
   public getCpf(): string {
     return this.cpf;

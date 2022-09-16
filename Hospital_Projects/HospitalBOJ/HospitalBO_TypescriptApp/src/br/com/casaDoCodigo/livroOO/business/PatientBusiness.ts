6fbbc818d90b604e032056c88619eb267e7bd22b
alexandregarskemachado <alexandre.machado@compasso.com.br>
2022-09-16 16:51:08 -0300

    Create Patient Service

--- Hospital_Projects/HospitalBOJ/HospitalBO_TypescriptApp/src/br/com/casaDoCodigo/livroOO/business/PatientBusiness.ts
+++ Hospital_Projects/HospitalBOJ/HospitalBO_TypescriptApp/src/br/com/casaDoCodigo/livroOO/business/PatientBusiness.ts
@@ -3,7 +3,7 @@ import { Plan } from "../entities/Plan";
 import { AddressConversor } from "../utils/AddressConversor";
 import { DateConversor } from "../utils/DateConversor";
 import { JsonDB, Config } from "node-json-db";
-
+import fs from "fs";
 export class PatientBusiness {
   private db: any;
 
@@ -19,7 +19,6 @@ export class PatientBusiness {
       dateOfBirth,
       plan: { planName, monthlyPayment },
     } = patient;
-
     const newPatient: Patient = new Patient(
       cpf,
       new Plan(planName, monthlyPayment),
@@ -27,13 +26,34 @@ export class PatientBusiness {
       new DateConversor().dateConverter(dateOfBirth),
       new AddressConversor().convertAddress(address)
     );
-    await this.db.push(
-      "/patients[]",
-      { id: `${newPatient.getCpf()}`, patient: newPatient },
-      true
-    );
+    let allPatients = await this.db.getData("/patients");
+    let canAdd = true;
+    allPatients.forEach(async (patient: { id: any }) => {
+      if (patient.id == newPatient.getCpf()) {
+        canAdd = false;
+      }
+    });
+    canAdd &&
+      (await this.db.push(
+        "/patients[]",
+        { id: `${newPatient.getCpf()}`, patient: newPatient },
+        true
+      ));
   }
-  show() {}
+
+  async show(query: any) {
+    const patientsArray = await this.db.getData("/patients");
+    let showData = false;
+    let patientData;
+    patientsArray.forEach(async (patient: { id: any }) => {
+      if (patient.id === query) {
+        showData = true;
+        patientData = patient;
+      }
+    });
+    return patientData;
+  }
+
   async showAll() {
     try {
       return await this.db.getData("/patients");
@@ -41,5 +61,30 @@ export class PatientBusiness {
       return error;
     }
   }
-  update() {}
+
+  async update(patient: any) {
+    const {
+      cpf,
+      name,
+      address,
+      dateOfBirth,
+      plan: { planName, monthlyPayment },
+    } = patient;
+    const newPatient: Patient = new Patient(
+      cpf,
+      new Plan(planName, monthlyPayment),
+      name,
+      new DateConversor().dateConverter(dateOfBirth),
+      new AddressConversor().convertAddress(address)
+    );
+
+    let content = JSON.parse(fs.readFileSync("hospitalDataBase.json", "utf-8"));
+    content.patients.forEach(async (oldPatient: any, key: any) => {
+      if (oldPatient.id == newPatient.getCpf()) {
+        content.patients[key].patient = newPatient;
+      }
+    });
+    fs.writeFileSync("hospitalDataBase.json", JSON.stringify(content));
+    await this.db.reload();
+  }
 }
